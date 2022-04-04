ebec8308f916c878d63328b6a3873f25a2b23a69
Icaro Miranda <icaromirandajustino@gmail.com>
2022-04-04 09:49:19 -0300

    prototipo sidebar

---
+++ src/Components/SidebarData.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import HomeIcon from '@mui/icons-material/Home';
+import EventIcon from '@mui/icons-material/Event';
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import AddToQueueIcon from '@mui/icons-material/AddToQueue';
+export const SidebarData = [
+    {
+        title: "Home",
+        icon: 7,
+        link: "/home"
+    },
+    {
+        title: "Perfil",
+        icon: 8,
+        link: "/perfil"
+    },
+    {
+        title: "Agenda",
+        icon: 9,
+        link: "/agenda"
+    },
+    {
+        title: "Guia",
+        icon: 10,
+        link: "/guia"
+    }
+]
